faa27add60a7c050cff9a9da6d3b792166ef1f5b
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-04-20 18:18:34 +0200

    Update script to normalize identifiers and add artifact

--- scripts/get_selventa_transitivities.py
+++ scripts/get_selventa_transitivities.py
@@ -2,26 +2,90 @@
 
 """Get transitive relations from the Selventa BEL content."""
 
+import bioregistry
 import click
+import logging
 import pandas as pd
+import pyobo
+from more_click import verbose_option
+from pyobo.xrefdb.sources.famplex import _get_famplex_df
+from tqdm.autonotebook import tqdm
+from typing import Tuple, Union
+
 import pybel
 import pybel.constants as pc
 import pystow
-from more_click import verbose_option
 from pybel.dsl import BaseConcept
-from tqdm.autonotebook import tqdm
+
+logger = logging.getLogger(__name__)
 
 URL = 'https://github.com/cthoyt/selventa-knowledge/raw/master/selventa_knowledge/large_corpus.bel'
 
 module = pystow.module('causal_precedence_training', 'selventa')
+
+fplx_df = _get_famplex_df()
+bel_fplx = dict(fplx_df.loc[fplx_df['target_ns'] == 'BEL', ['target_id', 'source_id']].values)
 
 
 @click.command()
 @verbose_option
 @click.option('--force', is_flag=True)
 def main(force: bool):
-    df = get_dataframe(force=force)
+    df = get_normalized_dataframe(force=force)
     click.echo(df.head())
+
+
+def get_normalized_dataframe(force: bool = False) -> pd.DataFrame:
+    cache_path = module.join(name='large_corpus_norm.tsv')
+    df = get_dataframe(force=force)
+
+    for letter in 'abc':
+        df[[f'{letter}.prefix', f'{letter}.identifier', f'{letter}.name']] = [
+            get_identifier(namespace, name)
+            for namespace, name in df[[f'{letter}.prefix', f'{letter}.name']].values
+        ]
+    df.to_csv(cache_path, sep='\t', index=False)
+    return df
+
+
+MISSING_NAMESPACE = set()
+MISSING = set()
+
+
+def get_identifier(namespace: str, name: str) -> Union[Tuple[str, None, str], Tuple[str, str, str]]:
+    if namespace in {'SFAM', 'SCOMP'}:
+        return 'fplx', bel_fplx.get(name), name
+    if namespace in {'SCHEM', 'CHEBI'}:
+        prefix, identifier, name = pyobo.ground('chebi', name)
+        return prefix or namespace, identifier, name
+
+    norm_namespace = bioregistry.normalize_prefix(namespace)
+    if norm_namespace is None:
+        raise ValueError(f'could not normalize {namespace}')
+    namespace = norm_namespace
+
+    if namespace in MISSING_NAMESPACE:
+        return namespace, None, name
+    try:
+        name_id_mapping = pyobo.get_name_id_mapping(namespace)
+    except:
+        logger.info('missing namespace: %s', namespace)
+        MISSING_NAMESPACE.add(namespace)
+        return namespace, None, name
+
+    if name_id_mapping:
+        identifier = name_id_mapping.get(name)
+        if identifier:
+            return namespace, identifier, name
+        elif (namespace, name) not in MISSING:
+            MISSING.add((namespace, name))
+            logger.debug('missing lookup for %s ! %s', namespace, name)
+            return namespace, None, name
+    elif namespace not in MISSING_NAMESPACE:
+        logger.info('empty namespace: %s', namespace)
+        MISSING_NAMESPACE.add(namespace)
+
+    return namespace, None, name
 
 
 def get_dataframe(force: bool = False) -> pd.DataFrame:
@@ -80,6 +144,7 @@ def get_dataframe(force: bool = False) -> pd.DataFrame:
 
 
 def get_graph(force: bool = False) -> pybel.BELGraph:
+    """Get the Selventa large corpus as a BEL Graph."""
     cache_path = module.join(name='large_corpus.bel.pickle')
     if cache_path.exists() and not force:
         return pybel.load(cache_path)
