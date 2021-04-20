ec78c4ea53e01a8c63be066e06f2357c585bd920
steppi <albert_steppi@hms.harvard.edu>
2021-04-19 23:10:56 -0400

    Improve SIGNOR download function

--- causal_precedence_training/downloads.py
+++ causal_precedence_training/downloads.py
@@ -1,28 +1,27 @@
-"""Functions for downloading datafiles on which results depend.
+"""Functions for downloading datafiles
 """
 
 import os
 import boto3
 import shutil
 import tarfile
-import tempfile
 import causal_precedence_training.locations as locations
 
 
 def download_signor_pathways():
-    """Download SIGNOR pathways
+    """Download SIGNOR pathways into data directory
     """
     client = boto3.client('s3')
-    with tempfile.TemporaryDirectory() as tmpdir:
-        temporary_pathways_location = os.path.join(tmpdir,
-                                                   'SIGNOR_pathways.tar.gz')
-        client.download_file(locations.S3_BUCKET,
-                             f'{locations.S3_DATA_PATH}/SIGNOR_pathways.tar.gz',
-                             temporary_pathways_location)
-        with tarfile.open(temporary_pathways_location) as tar:
-            tar.extractall(path=locations.LOCAL_DATA_HOME)
-            # Change folder name to that listed in
-            # causal_precedence_training.locations
-            shutil.move(os.path.join(locations.LOCAL_DATA_HOME,
-                                     'SIGNOR_pathways'),
-                        locations.SIGNOR_PATHWAYS)
+    pathways_tarball_location = os.path.join(locations.LOCAL_DATA_HOME,
+                                             'SIGNOR_pathways.tar.gz')
+    client.download_file(locations.S3_BUCKET,
+                         f'{locations.S3_DATA_PATH}/SIGNOR_pathways.tar.gz',
+                         pathways_tarball_location)
+    with tarfile.open(pathways_tarball_location) as tar:
+        tar.extractall(path=locations.LOCAL_DATA_HOME)
+    os.remove(pathways_tarball_location)
+    # Change folder name to that listed in
+    # causal_precedence_training.locations
+    shutil.move(os.path.join(locations.LOCAL_DATA_HOME,
+                             'SIGNOR_pathways'),
+                locations.SIGNOR_PATHWAYS)
