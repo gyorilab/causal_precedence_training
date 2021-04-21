0459e8e9ed6f041b2ec7caa713c835609c88c143
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-04-21 15:31:17 +0200

    Create __init__.py

---
+++ src/causal_precedence_training/resources/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Causal precedence training resources."""
+
+import os
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+SELVENTA_LARGE_CORPUS = os.path.join(HERE, 'selventa_large_corpus.tsv')
